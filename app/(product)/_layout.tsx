725c3dbd19944d9db864867896196b99f4fe1afe
Ikenna Richard <oguejioforrichard@gmail.com>
2025-10-26 14:47:30 +0100

    Remove boilerplate code

---
+++ app/(product)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+function ProductLayout() {
+  return <Stack>
+    <Stack.Screen name="index" />
+  </Stack>;
+}
+
+export default ProductLayout;
